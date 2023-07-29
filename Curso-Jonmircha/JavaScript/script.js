2b27a83abd8d9693519434a58aa76e10c3657359
Mauricio Molina <maupena9@gmail.com>
2023-07-29 11:29:04 -0500

    new changes

--- Curso-Jonmircha/JavaScript/script.js
+++ Curso-Jonmircha/JavaScript/script.js
@@ -1,8 +1,29 @@
-function redireccionar() {
-    var selectElement = document.getElementById("opciones");
-    var selectedValue = selectElement.value;
-    if (selectedValue !== "") {
-      window.location.href = selectedValue;
+const imageContainer = document.querySelector(".image-container");
+const images = document.querySelectorAll(".image-container img");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+
+let currentIndex = 0;
+
+leftArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateGallery();
+});
+
+rightArrow.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateGallery();
+});
+
+function updateGallery() {
+  const translateValue = -currentIndex * (images[0].width + 10);
+  imageContainer.style.transform = `translateX(${translateValue}px)`;
+
+  images.forEach((img, index) => {
+    if (index === currentIndex) {
+      img.style.transform = "scale(1.2)";
+    } else {
+      img.style.transform = "scale(1)";
     }
-  }
-  
+  });
+}
